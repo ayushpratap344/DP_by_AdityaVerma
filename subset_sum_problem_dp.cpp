9e9ec3fca58c155b9495e46671e0089a8d332116
Ayush Pratap Singh <ayushpratap344@gmail.com>
2021-12-10 10:01:41 +0530

    Subset Sum [variation of 0 1 Knapsack]

--- subset_sum_problem_dp.cpp
+++ subset_sum_problem_dp.cpp
@@ -1,39 +1,82 @@
-// Subset Sum problem
-
 #include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+
 using namespace std;
+#define maxi 1000000000000000000
 
-bool isSubsetPoss(int arr[], int n, int sum) {
-	bool t[n + 1][sum + 1]; // DP - matrix
-	// initialization
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= sum; j++) {
-			if (i == 0)
-				t[i][j] = false;
-			if (j == 0)
-				t[i][j] = true;
-		}
-	}
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= sum; j++) {
-			if (arr[i - 1] <= j)
-				t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
-			else
-				t[i][j] = t[i - 1][j];
-		}
-	}
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
 
-	return t[n][sum];
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+typedef unordered_map<ll, ll> unmap;
+typedef vector<vl> vll;
+
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a, b) for (ll i = a; i < (b); i++)
+#define repdec(i, a, b) for (ll i = (a)-1; i >= b; i--)
+#define trav(a, x) for (auto &a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define pf push_front
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+bool subset(int arr[], int n, int sum)
+{
+    // code here
+
+    bool dp[n + 1][sum + 1];
+    for (int i = 0; i < n + 1; i++)
+        dp[i][0] = true;
+    for (int i = 1; i < sum + 1; i++)
+        dp[0][i] = false;
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < sum + 1; j++)
+        {
+            if (arr[i - 1] <= j)
+                dp[i][j] = (dp[i - 1][j - arr[i - 1]] || dp[i - 1][j]);
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+
+    return dp[n][sum];
 }
 
-signed main() {
-	int n; cin >> n;
-	int arr[n];
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	int sum; cin >> sum;
+int main()
+{
+    int arr[5] = {2, 3, 7, 8, 10};
+    for (int i = 1; i <= 15; i++)
+        cout << i << '\t' << subset(arr, 5, i) << endl;
 
-	isSubsetPoss(arr, n, sum) ? cout << "Yes\n" : cout << "No\n";
-	return 0;
+    // 1 and 0 will be printed respectively for 1 o 15
+    // 1->Subset sum is possible
+    // 0->Subset sum isn't possible
+    return 0;
 }
