8c461839e8b072a094b870603f14dbb1400b7ea9
Ayush Pratap Singh <ayushpratap344@gmail.com>
2021-12-10 19:13:28 +0530

    Equal Sum Partition Leetcode

---
+++ EqualSumPartition_Leetcode.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    bool SubsetSum(vector<int> &nums, int sum)
+    {
+        // code here
+        int n = nums.size();
+        bool dp[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++)
+            dp[i][0] = true;
+        for (int i = 1; i < sum + 1; i++)
+            dp[0][i] = false;
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = 1; j < sum + 1; j++)
+            {
+                if (nums[i - 1] <= j)
+                    dp[i][j] = (dp[i - 1][j - nums[i - 1]] || dp[i - 1][j]);
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[n][sum];
+    }
+
+    bool canPartition(vector<int> &nums)
+    {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2)
+            return false;
+
+        else
+        {
+            return SubsetSum(nums, sum / 2);
+        }
+    }
+};
